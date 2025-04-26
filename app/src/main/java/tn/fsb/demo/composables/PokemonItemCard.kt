1071bb87ea07bcc047a4b49fb76f2125ca765741
oaroua <aroua.oussema@gmail.com>
2025-04-26 12:28:30 +0100

    Feat: Add Blur implementation in PokemonItemCard

--- app/src/main/java/tn/fsb/demo/composables/PokemonItemCard.kt
+++ app/src/main/java/tn/fsb/demo/composables/PokemonItemCard.kt
@@ -1,13 +1,18 @@
 package tn.fsb.demo.composables
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -22,24 +27,34 @@ import tn.fsb.demo.ui.theme.FSBDemoTheme
 data class PokemonItemCardModel(
     val pokemonName: String,
     val pokemonImage: String,
+    val color: Long,
+    var isBlur: Boolean = true
 )
+
+private const val TAG = "PokemonItemCard"
 
 @Composable
 fun PokemonItemCard(
     pokemon: PokemonItemCardModel,
+    onClick : () -> Unit
 ) {
     Card(
+        onClick = onClick,
         modifier = Modifier
             .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .blur(if (pokemon.isBlur) 16.dp else 0.dp)
     ) {
         Column(
-            modifier = Modifier.background(Color(0xFF68A890)),
+            modifier = Modifier
+                .background(Color(pokemon.color))
+                .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             AsyncImage(
                 model = pokemon.pokemonImage,
                 contentDescription = null,
-                placeholder = painterResource(R.drawable.pokemon),
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
             )
 
             Text(
@@ -66,7 +81,8 @@ fun PokemonItemCardPreview() {
             PokemonItemCardModel(
                 pokemonName = "Pikachu",
                 pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+                color = 0xFFEEFFFF,
             )
-        )
+        ){}
     }
 }
--- app/src/main/java/tn/fsb/demo/screen/PokemonListScreen.kt
+++ app/src/main/java/tn/fsb/demo/screen/PokemonListScreen.kt
@@ -3,12 +3,14 @@ package tn.fsb.demo.screen
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import tn.fsb.demo.composables.PokemonItemCard
@@ -25,40 +27,102 @@ fun PokemonListScreen() {
             )
         }
     ) { padding ->
+        val list = remember {
+            mutableStateOf(
+                listOf(
+                    PokemonItemCardModel(
+                        pokemonName = "bulbasaur",
+                        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+                        color = 0xFFADC542
+                    ),
+                    PokemonItemCardModel(
+                        pokemonName = "ivysaur",
+                        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+                        color = 0xFFADC542
+
+                    ),
+                    PokemonItemCardModel(
+                        pokemonName = "venusaur",
+                        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+                        color = 0xFFADC542
+
+                    ),
+                    PokemonItemCardModel(
+                        pokemonName = "charmander",
+                        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+                        color = 0xFFADC542
+
+                    ),
+                    PokemonItemCardModel(
+                        pokemonName = "charmeleon",
+                        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+                        color = 0xFFADC542
+
+                    ),
+                    PokemonItemCardModel(
+                        pokemonName = "charizard",
+                        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+                        color = 0xFFFDC554
+                    )
+                )
+            )
+        }
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             modifier = Modifier.padding(padding)
         ) {
-            items(pokemonList) { item ->
-                PokemonItemCard(item)
+            itemsIndexed(list.value) { i, item ->
+                PokemonItemCard(
+                    pokemon = item,
+                    onClick = {
+                        list.value = list.value.mapIndexed { index, item ->
+                            if (i == index) item.copy(
+                                isBlur = !item.isBlur
+                            ) else item
+                        }
+                    }
+                )
             }
         }
     }
 }
 
+private const val TAG = "PokemonListScreen"
+
 val pokemonList = listOf(
     PokemonItemCardModel(
         pokemonName = "bulbasaur",
-        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        color = 0xFFADC542
+
     ),
     PokemonItemCardModel(
         pokemonName = "ivysaur",
-        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+        color = 0xFFADC542
+
     ),
     PokemonItemCardModel(
         pokemonName = "venusaur",
-        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"
+        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+        color = 0xFFADC542
+
     ),
     PokemonItemCardModel(
         pokemonName = "charmander",
-        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+        color = 0xFFADC542
+
     ),
     PokemonItemCardModel(
         pokemonName = "charmeleon",
-        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"
+        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+        color = 0xFFADC542
+
     ),
     PokemonItemCardModel(
         pokemonName = "charizard",
-        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
+        pokemonImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+        color = 0xFFFDC554
     )
 )
